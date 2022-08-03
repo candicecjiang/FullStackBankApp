19600b25f0dd8a3a3fd7b0e5bc36597a7243a2ad
Candice Jiang <93559324+candicecjiang@users.noreply.github.com>
2022-08-03 10:32:41 -0400

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = 'mongodb+srv://admin:bankapppw@cluster0.beomj.mongodb.net/?retryWrites=true&w=majority';
 let db            = null;
  
 // connect to mongo
